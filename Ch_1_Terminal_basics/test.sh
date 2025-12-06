f85c7dbc348b13f8382b2e45f31fb25eac9629c6
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-12-06 12:21:24 +0530

    adding bash code

---
+++ Ch_1_Terminal_basics/test.sh
@@ -0,0 +1,15 @@
+echo this is starting 
+a=10
+b=20
+c="PushanVerma"
+echo $a : a 
+echo $b : b
+echo $((a+b)) : this is the addition of two numbers 
+echo $c : this is string 
+
+echo this is ending 
+
+echo --------------------------------------
+
+
+echo pwd
